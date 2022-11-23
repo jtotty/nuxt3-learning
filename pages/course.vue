22b0f0571af5fbb10f6195f188027b7326e3e16b
jtotty <jt.totty1991@gmail.com>
2022-11-23 18:11:40 +0000

    Styling for course and lesson page.

--- pages/course.vue
+++ pages/course.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
-  >
+  <div class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center">
     <div class="prose mb-12">
       <h1>
         <span class="font-medium">
@@ -10,13 +8,10 @@
         </span>
       </h1>
     </div>
-
+    
     <div class="flex flex-row justify-center flex-grow">
-      <div
-        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col"
-      >
+      <div class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col">
         <h3>Chapters</h3>
-        <!-- All the lessons for the course listed here -->
       </div>
 
       <div class="prose p-12 bg-white rounded-md w-[65ch]">
