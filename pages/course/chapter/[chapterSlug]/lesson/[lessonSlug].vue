43f75e15d18946e4e2537ff63b6aa3494511bf28
jtotty <jt.totty1991@gmail.com>
2022-11-24 23:57:42 +0000

    Middleware

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,34 +1,23 @@
 <template>
-    <div>
-        <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
-            Lesson {{ chapter.number }} - {{ lesson.number }}
-        </p>
-        <h2 class="my-0">{{ lesson.title }}</h2>
-        <div class="flex space-x-4 mt-2 mb-8">
-            <NuxtLink
-                v-if="lesson.sourceUrl"
-                class="font-normal text-md text-gray-500"
-                :to="lesson.sourceUrl"
-            >
-                Download Source Code
-            </NuxtLink>
+  <div>
+    <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
+      Lesson {{ chapter.number }} - {{ lesson.number }}
+    </p>
+    <h2 class="my-0">{{ lesson.title }}</h2>
+    <div class="flex space-x-4 mt-2 mb-8">
+      <NuxtLink v-if="lesson.sourceUrl" class="font-normal text-md text-gray-500" :to="lesson.sourceUrl">
+        Download Source Code
+      </NuxtLink>
 
-            <NuxtLink
-                v-if="lesson.downloadUrl"
-                class="font-normal text-md text-gray-500"
-                :to="lesson.downloadUrl"
-            >
-                Download Video
-            </NuxtLink>
-        </div>
-
-        <VideoPlayer v-if="lesson.videoId" :videoId="lesson.videoId" />
-        <p>{{ lesson.text }}</p>
-        <LessonCompleteButton
-            :model-value="isLessonComplete"
-            @update:model-value="toggleComplete"
-        />
+      <NuxtLink v-if="lesson.downloadUrl" class="font-normal text-md text-gray-500" :to="lesson.downloadUrl">
+        Download Video
+      </NuxtLink>
     </div>
+
+    <VideoPlayer v-if="lesson.videoId" :videoId="lesson.videoId" />
+    <p>{{ lesson.text }}</p>
+    <LessonCompleteButton :model-value="isLessonComplete" @update:model-value="toggleComplete" />
+  </div>
 </template>
 
 <script setup>
@@ -38,44 +27,45 @@ const route = useRoute();
 // (*) definePageMeta has no access outside it's scope
 // Hence why we have to redeclare course, chapter, and lesson
 definePageMeta({
-    validate({ params }) {
-        const course = useCourse();
-        const chapter = course.chapters.find((chapter) => chapter.slug === params.chapterSlug);
+  middleware: [
+    function (to, from) {
+      const course = useCourse();
+      const chapter = course.chapters.find((chapter) => chapter.slug === to.params.chapterSlug);
 
-        if (!chapter) {
-            throw createError({
-                statusCode: 404,
-                message: 'Chapter not found'
-            });
-        }
+      if (!chapter) {
+        throw abortNavigation(createError({
+          statusCode: 404,
+          message: 'Chapter not found'
+        }));
+      }
 
-        const lesson = chapter.lessons.find((lesson) => lesson.slug === params.lessonSlug);
+      const lesson = chapter.lessons.find((lesson) => lesson.slug === to.params.lessonSlug);
 
-        if (!lesson) {
-            throw createError({
-                statusCode: 404,
-                message: 'Lesson not found'
-            });
-        }
-
-        return true;
-    }
+      if (!lesson) {
+        throw abortNavigation(createError({
+          statusCode: 404,
+          message: 'Lesson not found'
+        }));
+      }
+    },
+    'auth'
+  ]
 });
 
 if (route.params.lessonSlug === '3-typing-component-events') {
-    console.log(route.params.paramthatdoesnotexist.capitalizeIsNotAMethod());
+  console.log(route.params.paramthatdoesnotexist.capitalizeIsNotAMethod());
 }
 
 const chapter = computed(() => {
-    return course.chapters.find((chapter) => chapter.slug === route.params.chapterSlug);
+  return course.chapters.find((chapter) => chapter.slug === route.params.chapterSlug);
 });
 
 const lesson = computed(() => {
-    return chapter.value.lessons.find((lesson) => lesson.slug === route.params.lessonSlug);
+  return chapter.value.lessons.find((lesson) => lesson.slug === route.params.lessonSlug);
 });
 
 const title = computed(() => {
-    return `${lesson.value.title} - ${course.title}`;
+  return `${lesson.value.title} - ${course.title}`;
 });
 
 useHead({ title });
@@ -83,22 +73,22 @@ useHead({ title });
 const progress = useLocalStorage('progress', []);
 
 const isLessonComplete = computed(() => {
-    if (!progress.value[chapter.value.number - 1]) {
-        return false;
-    }
+  if (!progress.value[chapter.value.number - 1]) {
+    return false;
+  }
 
-    if (!progress.value[chapter.value.number - 1][lesson.value.number - 1]) {
-        return false;
-    }
+  if (!progress.value[chapter.value.number - 1][lesson.value.number - 1]) {
+    return false;
+  }
 
-    return progress.value[chapter.value.number - 1][lesson.value.number - 1];
+  return progress.value[chapter.value.number - 1][lesson.value.number - 1];
 });
 
 const toggleComplete = () => {
-    if (!progress.value[chapter.value.number - 1]) {
-        progress.value[chapter.value.number - 1] = [];
-    }
+  if (!progress.value[chapter.value.number - 1]) {
+    progress.value[chapter.value.number - 1] = [];
+  }
 
-    progress.value[chapter.value.number - 1][lesson.value.number - 1] = !isLessonComplete.value;
+  progress.value[chapter.value.number - 1][lesson.value.number - 1] = !isLessonComplete.value;
 };
 </script>
