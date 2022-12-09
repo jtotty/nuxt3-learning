7dab4a5a7c3aa4771b9adc1935d3f778dc6ede41
jtotty <jt.totty1991@gmail.com>
2022-12-09 18:00:01 +0000

    Finish Github OAuth and middleware

--- pages/login.vue
+++ pages/login.vue
@@ -12,10 +12,24 @@
 
 <script lang="ts" setup>
 const { title } = useCourse();
+const { query } = useRoute();
 const { auth } = useSupabaseClient();
+const user = useSupabaseUser();
+
+watchEffect(async () => {
+  if (user.value) {
+    await navigateTo(query.redirectTo as string, {
+      replace: true
+    });
+  }
+});
 
 const login = async () => {
-  const { error } = await auth.signInWithOAuth({ provider: 'github' });
+  const redirectTo = `${window.location.origin}${query.redirectTo}`;
+  const { error } = await auth.signInWithOAuth({
+    provider: 'github',
+    options: { redirectTo }
+  });
 
   if (error) {
     console.log(error)
