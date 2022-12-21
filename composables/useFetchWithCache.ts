af128cfc847015dbf1242ff2b542897a5e6b00a7
jtotty <jt.totty1991@gmail.com>
2022-12-21 18:04:28 +0000

    Added prisma migrations and seed

--- composables/useFetchWithCache.ts
+++ composables/useFetchWithCache.ts
@@ -2,11 +2,10 @@ import { StorageSerializers } from '@vueuse/core';
 
 export default async <T>(url: string) => {
   const cached = useSessionStorage<T>(url, null, {
-      // By passing null as default it can't automatically
-      // determine which serializer to use
-      serializer: StorageSerializers.object
-    }
-  );
+    // By passing null as default it can't automatically
+    // determine which serializer to use
+    serializer: StorageSerializers.object
+  });
 
   if (!cached.value) {
     const { data, error } = await useFetch<T>(url);
@@ -19,8 +18,6 @@ export default async <T>(url: string) => {
     }
 
     cached.value = data.value as T;
-  } else {
-    console.log(`Getting value from cache for ${url}`);
   }
 
   return cached;
